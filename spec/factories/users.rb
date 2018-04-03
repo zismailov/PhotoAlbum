8f3cd73a9faafc791d55da7f69d63f14a8aabe22
zulkarnay <bedelbaev@gmail.com>
2018-04-03 13:01:38 +0600

    slim typo fixed

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -13,7 +13,7 @@ FactoryBot.define do
     confirmed_at nil
 
     after(:create) do |user|
-      user.update(confirmation_sent_at: 1.days.ago)
+      user.update(confirmation_sent_at: 1.day.ago)
     end
   end
 end
