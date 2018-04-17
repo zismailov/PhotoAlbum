7de3b4883d2082c120ef859df712be78b3520347
zulkarnay <bedelbaev@gmail.com>
2018-04-17 14:09:15 +0600

    work in progress with photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,29 +1,26 @@
 class PhotosController < ApplicationController
   before_action :authenticate_user!
 
+  before_action :set_album, only: %w[index create]
+  before_action :set_photo, only: %w[show update destroy]
+
   respond_to :html
 
-  expose(:photo)
-
   def index
-    @album = Album.find(params[:album_id])
     @photos = @album.photos
   end
 
   def create
-    @album = Album.find(params[:album_id])
-    @photo = @album.photos.new
+    @photo = @album.photos.new title: photo_title
 
-    @photo.title = File.basename(params[:filename], File.extname(params[:filename]))
-
-    @photo.remote_picture_url = URI.decode_www_form photo_params[:remote_picture_url]
+    @photo.remote_picture_url = photo_remote_url
     @photo.save
 
     Amazon.delete_tmp_file(URI.decode_www_form(params[:filepath]))
   end
 
   def update
-    if photo.update(photo_params)
+    if @photo.update(photo_params)
       redirect_to photo, notice: "Photo was successfully updated."
     else
       render :edit
@@ -31,10 +28,26 @@ class PhotosController < ApplicationController
   end
 
   def destroy
-    photo.destroy
+    @photo.destroy
   end
 
   private
+
+  def photo_title
+    File.basename(params[:filename], File.extname(params[:filename]))
+  end
+
+  def photo_remote_url
+    URI.decode_www_form photo_params[:remote_picture_url]
+  end
+
+  def set_album
+    @album = Album.find(params[:album_id])
+  end
+
+  def set_photo
+    @photo = Photo.find(params[:id])
+  end
 
   def photo_params
     params.require(:photo).permit(:title, :description, :album_id, :picture, :remote_picture_url)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :albums, except: %i[show] do
-    resources :photos, except: %i[new show], shallow: true
+    resources :photos, except: %i[new], shallow: true
   end
 
   devise_for :users
