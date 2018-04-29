60b313a1bc949ddb092a660271d935b63f8a0583
Zulkar <bedelbaev@gmail.com>
2018-04-29 15:01:16 +0600

    code quality fixes

--- lib/amazon.rb
+++ lib/amazon.rb
@@ -2,14 +2,23 @@ class Amazon
   TMP_BUCKET = "photoalbum-tmp".freeze
 
   def self.delete_tmp_file(filepath)
-    Aws.config.update(
-      region: ENV["AWS_REGION"],
-      credentials: Aws::Credentials.new(ENV["S3_ACCESS_KEY_ID"], ENV["S3_SECRET_KEY_ID"])
-    )
+    Aws.config.update parameters
 
     s3 = Aws::S3::Resource.new
     bucket = s3.bucket("photoalbum-tmp")
 
     bucket.object(filepath.gsub("/#{TMP_BUCKET}/", "")).delete
+  end
+
+  def region
+    ENV["AWS_REGION"]
+  end
+
+  def credentials
+    Aws::Credentials.new(ENV["S3_ACCESS_KEY_ID"], ENV["S3_SECRET_KEY_ID"])
+  end
+
+  def parameters
+    { region: region, credentials: credentials }
   end
 end
