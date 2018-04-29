35716ab1644b2974638e27bde0be196e476448a4
Zulkar <bedelbaev@gmail.com>
2018-04-29 14:51:42 +0600

    add new matchers config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -25,7 +25,12 @@ end
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
+    # Choose a test framework:
     with.test_framework :rspec
-    with.library :rails
+
+    # Choose one or more libraries:
+    with.library :active_record
+    with.library :active_model
+    with.library :action_controller
   end
 end
