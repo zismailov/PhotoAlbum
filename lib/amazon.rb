0f55b3330a066fa0927e4c7ec8db935660cdbb31
Zulkar <bedelbaev@gmail.com>
2018-04-29 15:40:40 +0600

    fix for tmp removing

--- lib/amazon.rb
+++ lib/amazon.rb
@@ -2,23 +2,14 @@ class Amazon
   TMP_BUCKET = "photoalbum-tmp".freeze
 
   def self.delete_tmp_file(filepath)
+    region = ENV["AWS_REGION"]
+    credentials = Aws::Credentials.new(ENV["S3_ACCESS_KEY_ID"], ENV["S3_SECRET_KEY_ID"])
+    parameters = { region: region, credentials: credentials }
     Aws.config.update parameters
 
     s3 = Aws::S3::Resource.new
     bucket = s3.bucket("photoalbum-tmp")
 
     bucket.object(filepath.gsub("/#{TMP_BUCKET}/", "")).delete
-  end
-
-  def region
-    ENV["AWS_REGION"]
-  end
-
-  def credentials
-    Aws::Credentials.new(ENV["S3_ACCESS_KEY_ID"], ENV["S3_SECRET_KEY_ID"])
-  end
-
-  def parameters
-    { region: region, credentials: credentials }
   end
 end
