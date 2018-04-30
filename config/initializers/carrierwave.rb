edbb8ba28c959c2f8e20dea6a2e7ae3b433664f5
Zulkar <bedelbaev@gmail.com>
2018-04-30 13:58:54 +0600

    Clouffront production settings

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,16 @@
 CarrierWave.configure do |config|
   config.storage    = :aws
   config.aws_bucket = "photoalbum-#{Rails.env}"
-  config.aws_acl    = :private
+  config.aws_acl    = "public-read"
+
   config.aws_credentials = {
     access_key_id:     ENV["S3_ACCESS_KEY_ID"],
     secret_access_key: ENV["S3_SECRET_KEY_ID"]
   }
-  config.aws_authenticated_url_expiration = 60 * 60
-end
 
-config.asset_host = ENV["CLOUDFRONT_URL"]
+  config.asset_host = ENV["CLOUDFRONT_URL"] if Rails.env.production?
+
+  # config.aws_authenticated_url_expiration = 60 * 60
+end
 
 CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
