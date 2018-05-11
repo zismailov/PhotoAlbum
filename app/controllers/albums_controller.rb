9cae4ea563ec5d2f6f26470418d64ae243a95b6d
Zulkar <bedelbaev@gmail.com>
2018-05-11 23:55:10 +0600

    fix for photos order

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -22,7 +22,7 @@ class AlbumsController < ApplicationController
     album.save
     RefreshPhotosOrder.call(album: album)
 
-    respond_with album
+    render nothing: true, status: :ok
   end
 
   def destroy
--- app/decorators/photo_decorator.rb
+++ app/decorators/photo_decorator.rb
@@ -8,7 +8,9 @@ class PhotoDecorator < BaseDecorator
   def next_photo_id_from_album
     self_position = album.photos_order.index(id.to_s)
 
-    if self_position + 1 == album.photos_order.length
+    if self_position.nil?
+      nil
+    elsif self_position + 1 == album.photos_order.length
       album.photos_order.first
     else
       album.photos_order[self_position + 1]
@@ -18,7 +20,9 @@ class PhotoDecorator < BaseDecorator
   def previous_photo_id_from_album
     self_position = album.photos_order.index(id.to_s)
 
-    if self_position.zero?
+    if self_position.nil?
+      nil
+    elsif self_position.zero?
       album.photos_order.last
     else
       album.photos_order[self_position - 1]
