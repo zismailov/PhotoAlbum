6850ce1f0eea343ff31323d0a32b184a2b96aad6
Zulkar <bedelbaev@gmail.com>
2018-08-08 13:12:11 +0600

    Revert "full redesign started"
    This reverts commit 3050c8741fbdfc4ed6eb50f786ba1af73f33e049.

--- spec/support/devise_helpers.rb
+++ spec/support/devise_helpers.rb
@@ -2,6 +2,8 @@ module DeviseHelpers
   def sign_in(email, password)
     visit new_user_session_path
 
+    click_button t("actions.devise.sign_in")
+
     fill_form(
       :user,
       email: email,
